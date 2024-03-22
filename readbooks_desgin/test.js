3c30322e387ca39273d59e42c19a838b93630882
galaticwarrior123 <121226705+galaticwarrior123@users.noreply.github.com>
2024-03-22 16:20:02 +0700

    Add files via upload
    Upfile

---
+++ readbooks_desgin/test.js
@@ -0,0 +1,7 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Cấu hình tùy chỉnh
+    loop: true, // Lặp lại các slide
+    autoplay: {
+      delay: 3000, // Thời gian hiển thị mỗi slide (tính bằng mili giây)
+    },
+  });
